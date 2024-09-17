1536dfd25bbc29d53337e12ef4b3cfff95b3b392
Pulkit Jain <pulkitjainhk@gmail.com>
2024-09-17 23:41:49 +0530

    chore: Adding Data. Update styling for product list and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
 
+  const fetchProducts = async () => {
+    const res = await fetch(`https://dummyjson.com/products?limit=100`);
+    const data = await res.json();
+
+    if(data && data.products){
+      setProducts(data.products);
+    }
+
+    console.log(products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  },[]);
   return (
-    <>
-    </>
+    <div>
+      {products.length > 0 && (
+        <div className="products">
+          {products.map((product) => {
+            return (
+              <span className="products__single" key={product.id}>
+                <img src={product.thumbnail} alt={product.title} />
+                <span>{product.title}</span>
+              </span>
+            );
+          })}
+        </div>
+      )}
+    </div>
   )
 }
 
