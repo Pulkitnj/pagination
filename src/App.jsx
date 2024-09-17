b1cca4fd037c216cab747dc2673c84e17f90fa19
Pulkit Jain <pulkitjainhk@gmail.com>
2024-09-18 02:24:25 +0530

    chore: Update styling for product list and adding pagination

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const PageHandler = (page) => {
+    setPage(page);
+  }
 
   const fetchProducts = async () => {
     const res = await fetch(`https://dummyjson.com/products?limit=100`);
@@ -18,22 +23,54 @@ function App() {
   useEffect(() => {
     fetchProducts();
   },[]);
+
   return (
-    <div>
+    <div className="container">
+      <header className="header">
+        <h1>Pagination</h1>
+      </header>
       {products.length > 0 && (
         <div className="products">
-          {products.map((product) => {
+          {products.slice(page * 10 - 10, page * 10).map((product) => {
             return (
               <span className="products__single" key={product.id}>
                 <img src={product.thumbnail} alt={product.title} />
+                <b className="number">{product.id + " "}</b>
                 <span>{product.title}</span>
               </span>
             );
           })}
         </div>
       )}
+      {products.length > 0 && (
+        <div className="pagination-container">
+          <div className="pagination">
+            <span 
+              className={page > 1 ? "pagination__button" : "pagination__button pagination__disable"}
+              onClick={() => {
+                if (page > 1) setPage(page - 1);
+              }}>◀</span>
+            {[...Array(Math.ceil(products.length / 10))].map((_, index) => {
+              return (
+                <span
+                  className={page === index + 1 ? "pagination__button pagination__selected" : "pagination__button"}
+                  key={index}
+                  onClick={() => PageHandler(index + 1)}
+                >
+                  {index + 1}
+                </span>
+              );
+            })}
+            <span 
+              className={page < Math.ceil(products.length / 10) ? "pagination__button" : "pagination__button pagination__disable"}
+              onClick={() => {
+                if (page < Math.ceil(products.length / 10)) setPage(page + 1);
+              }}>▶</span>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
